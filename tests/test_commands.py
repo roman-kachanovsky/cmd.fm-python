d415e97935dbcf8b5132d54a4f072ed948ffabd5
Roman Kachanovsky <roman.kachanovsky@gmail.com>
2017-04-05 14:58:26 +0300

    Renamed some stuff test functions

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -17,25 +17,25 @@ class TestFm(unittest.TestCase):
     def create(self):
         return Fm(stdin=self.mock_stdin, stdout=self.mock_stdout)
 
-    def _clear_coloring(self, text):
+    def clear_coloring(self, text):
         return re.sub(r'\033\[[0-9]{1,2}m', '', text)
 
-    def _last_response(self):
-        return self._clear_coloring(self.mock_stdout.write.call_args_list[0][0][0])
+    def cli_response(self):
+        return self.clear_coloring(self.mock_stdout.write.call_args_list[0][0][0])
 
     def test_wrong_command(self):
         cli = self.create()
         self.assertFalse(cli.onecmd('wrong_command'))
-        self.assertEqual(self._last_response(), 'Unknown command wrong_command\n')
+        self.assertEqual(self.cli_response(), 'Unknown command wrong_command\n')
         self.mock_stdout.reset_mock()
 
     def test_play(self):
         cli = self.create()
         self.assertFalse(cli.onecmd('play'))
-        self.assertEqual(self._last_response(), 'debug: play/p command output\n')
+        self.assertEqual(self.cli_response(), 'debug: play/p command output\n')
         self.mock_stdout.reset_mock()
         self.assertFalse(cli.onecmd('p'))
-        self.assertEqual(self._last_response(), 'debug: play/p command output\n')
+        self.assertEqual(self.cli_response(), 'debug: play/p command output\n')
         self.mock_stdout.reset_mock()
 
     def test_quit(self):
